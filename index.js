adaf54d76e317bb12daf47e20810cf6b7cc9d542
Colin Walker <colinwalker270@gmail.com>
2019-08-23 02:54:42 +0000

    fix: Remove console log

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ async function run() {
     const client = new github.GitHub(token);
     const context = github.context;
 
-    console.log(context);
-
     await client.repos.createDeploymentStatus({
       ...context.repo,
       deployment_id: context.payload.deployment.id,
